ca85a8dbcdbcf0183e5d13f0829f1cdff425d6aa
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2023-09-28 19:58:06 -0300

    chore: remove calculator

--- models/calculator.js
+++
@@ -1,8 +0,0 @@
-function sum(number1, number2) {
-  if (typeof number1 !== "number" || typeof number2 !== "number") {
-    return "Error: one or both parameters are not numbers";
-  }
-  return number1 + number2;
-}
-
-exports.sum = sum;
